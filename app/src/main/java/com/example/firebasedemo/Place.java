aa5894b3ff62fb47c806b5799ed3ff95b724a9b4
KostyaRed444 <71414432+KostyaRed444@users.noreply.github.com>
2021-04-22 22:57:03 +0800

    Update Place.java

--- app/src/main/java/com/example/firebasedemo/Place.java
+++ app/src/main/java/com/example/firebasedemo/Place.java
@@ -1,10 +1,13 @@
 package com.example.firebasedemo;
 
+import android.content.Intent;
+import android.widget.EditText;
+
 public class Place {
     public String name;
     public double lat, lon;
 
-    public Place() {
+    public Place(EditText nameText, EditText latText, EditText lonText) {
         // для использования с Firebase требуется конструктор по умолчанию
     }
     public Place(String name, double lat, double lon) {
