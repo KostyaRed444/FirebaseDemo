6eaee7a50c2860b8daed1fa1f071d7dc3a85a823
KostyaRed444 <71414432+KostyaRed444@users.noreply.github.com>
2021-04-27 18:11:25 +0800

    Update Place.java

--- app/src/main/java/com/example/firebasedemo/Place.java
+++ app/src/main/java/com/example/firebasedemo/Place.java
@@ -5,17 +5,17 @@ import android.widget.EditText;
 
 public class Place {
     public String name;
-    public double lat, lon;
+    public double lan, lon;
 
     public Place(EditText nameText, EditText latText, EditText lonText) {
         // для использования с Firebase требуется конструктор по умолчанию
     }
-    public Place(String name, double lat, double lon) {
+    public Place(String name, double lan, double lon) {
         this.name = name;
-        this.lat = lat;
+        this.lan = lan;
         this.lon = lon;
     }
     public String toString () {
-        return "name: " + name + ", latitude: " + lat + ", longtitude: " + lon;
+        return "name: " + name + ", latitude: " + lan + ", longtitude: " + lon;
     }
 }
